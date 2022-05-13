57e23647f0d671019c7bc764ef9c76eec19a63fe
codebr <felipegabriel@users.noreply.github.com>
2022-05-13 16:09:19 -0300

    ola mundo node.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write("Ola!!"); //write a response to the client
+    res.write("Ola Mundo Node.js!!"); //write a response to the client
     res.end(); //end the response
   })
   .listen(3000); //the server object listens on port 8080
